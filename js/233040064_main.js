af040f14e60e12e7a0fd4324d12b4d534fa91e49
jojeyyyyyy <jhosuajeremiamatulessy@gmail.com>
2023-12-22 22:51:23 +0700

    selesai

--- js/233040064_main.js
+++ js/233040064_main.js
@@ -1,36 +1,44 @@
-// Toggle & Responsive Navigation
-const navSlide = () => {
-    const nburger = document.querySelector(".burger");
-    const navLists = document.querySelector("nav")
+// // Toggle & Responsive Navigation
+// const navSlide = () => {
+//     const nburger = document.querySelector(".burger");
+//     const navLists = document.querySelector("nav")
 
-burger.addEventListener("click", () => {
-    navLists.classList.toggle("nav-active");
-    burger.classList.toggle("toggle-burger");
-});
-};
+// burger.addEventListener("click", () => {
+//     navLists.classList.toggle("nav-active");
+//     burger.classList.toggle("toggle-burger");
+// });
+// };
 
-navSlide();
+// navSlide();
 
-// Clear form before unload
-window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-        form.reset();
-    }
-};
+// // Clear form before unload
+// window.onbeforeunload = () => {
+//     for (const form of document.getElementsByTagName("form")) {
+        // form.reset();
+//     }
+// };
 
 // Toogle class active
-const navbarNav = document.querySelector ('.navbar-nav')
-// Ketika hamburger menu diklik
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
-};
+// const navbarNav = document.querySelector ('.navbar-nav')
+// // Ketika hamburger menu diklik
+// document.querySelector('#hamburger-menu').onclick = () => {
+//     navbarNav.classList.toggle('active');
+// };
 
 //Klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+// document.addEventListener('click', function(e){
+//     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//         navbarNav.classList.remove('active');
+//     }
+// });
+
+
+// Navbar
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
 });
-
